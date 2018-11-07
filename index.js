b0b7c53d81a5d095c62dd98ef6bbaf0f90d04d7d
cyu8110 <cyu8110@gmail.com>
2018-11-07 16:47:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ function removeFromCart(item) {
   for (let i=0; i<cart.length; i++) {
     if (item === cart[i]) {
       splice(i,1);
+    } else {
+      return "That item is not in your cart."
     }
   }
     
