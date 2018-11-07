341d34905700f83cbb86d77052efdede61902ae8
cyu8110 <cyu8110@gmail.com>
2018-11-07 18:49:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === false) {
+  if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     cart = [];
