7c596bbbe9f702ea51dd40c86a693a00e993765c
cyu8110 <cyu8110@gmail.com>
2018-11-07 14:30:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,42 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = {itemName: item, itemPrice: Math.floor(Math.random() * 100)};
+  cart.push(newItem); 
+  return newItem.itemName + " has been added to your cart.";
 }
 
 function viewCart() {
-  // write your code here
+  var cartStem = "In your cart, you have "; 
+  var cartContent = [];
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  } else if (cart.length === 1 ) {
+    return cartStem + cart[0].itemName + " at $" + cart[0].itemPrice + ".";
+  } else {
+    for (let i = 0; i < cart.length-1; i++) {
+       cartContent.push(cart[i].itemName + " at $" + cart[i].itemPrice);
+    }
+       return cartStem + cartContent.join(", ") + ", and " + cart[cart.length-1].itemName + " at $" + 
+       cart[cart.length-1].itemPrice + ".";
+  }
 }
 
 function total() {
-  // write your code here
+  var cartCost = 0;
+  for (let i=0; i<cart.length; i++) {
+    cartCost = cartCost + cart[i].itemPrice;
+  }
+  return cartCost;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i=0; i<cart.length; i++) {
+    if (item === cart[i]) {
+      splice(i,1);
+    }
+  }
+    
 }
 
 function placeOrder(cardNumber) {
