334b749ece87f38846ef309b8b974beaceca9efe
cyu8110 <cyu8110@gmail.com>
2018-11-07 17:32:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,14 +41,13 @@ function total() {
 
 function removeFromCart(item) {
   for (let i=0; i<cart.length; i++) {
-    if (cart[i].itemName === item) {
+    if (i===cart.length) {
+      return "That item is not in your cart.";
+    } else if (cart[i].itemName === item) {
       cart.splice(i,1);
       return cart;
-    } else if (i===cart.length) {
-      return "That item is not in your cart.";
-    }   
+    }  
   }
-    
 }
 
 function placeOrder(cardNumber) {
